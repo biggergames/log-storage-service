939e3e69eafbf2a154938d432f0a7880ea8b8aae
orhan ocak <orhanocak@ip-10-10-14-227.ec2.internal>
2023-12-20 12:41:47 +0300

    feat-#1: adds package structure

---
+++ src/test/java/com/biggergames/backend/logstorageservice/LogStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biggergames.backend.logstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
